8ac629641e0d42c0826038509585e1733d6d3be9
kulw0019 <wansh020@gmail.com>
2026-08-17 06:55:26 +0000

    complete task 3 of 4

--- tutorials/t02/src/components/PlayerProfile.jsx
+++ tutorials/t02/src/components/PlayerProfile.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 function PlayerProfile() {
-  const player = { username: "PixelPioneer", level: 5, active: true };
+  const [player, setPlayer] = useState({
+    username: "PixelPioneer",
+    level: 5,
+    active: true
+  });
   const { username, level, active } = player;
+
+  const handleLevelUp = () => {
+    const updatedPlayer = { ...player, level: level + 1 };
+    setPlayer(updatedPlayer);
+  };
 
   return (
     <div className="profile-card">
       <h2>Player: {username}</h2>
       <p>Current Level: {level}</p>
+      <button onClick={handleLevelUp}>Gain XP</button>
     </div>
   );
 }
